645dedd484a5c3031c296ed307833b157949bcae
Johnny Gaddis <johnnygaddis777@gmail.com>
2018-01-23 14:54:33 -0700

    Steps 1, 2, 3, and 5 are done

--- ruby_lab.rb
+++ ruby_lab.rb
@@ -1,16 +1,16 @@
-
 #!/usr/bin/ruby
+
 ###############################################################
 #
 # CSCI 305 - Ruby Programming Lab
 #
-# <firstname> <lastname>
-# <email-address>
+# Johnny Gaddis
+# JohnnyGaddis777@gmail.com
 #
 ###############################################################
 
 $bigrams = Hash.new # The Bigram data structure
-$name = "<firstname> <lastname>"
+$name = "Johnny Gaddis"
 
 # function to process each line of a file and extract the song titles
 def process_file(file_name)
@@ -18,7 +18,19 @@ def process_file(file_name)
 
 	begin
 		IO.foreach(file_name) do |line|
-			# do something for each line
+			#Step 1: clear out everything before the title
+			title = line.sub(/.*>/, "")
+			#Step 2: clear out everything after the title
+			title.sub!(/(\(|\[|\{|\\|\/|_|-|:|"|`|\+|=|\*|feat\.).*/, "")
+			#Step 3: remove any and all punctuation
+			title.gsub!(/(\?|\¿|\!|\¡|\.|\;|\&|\@|\%|\#|\|)/, "")
+			#Step 4: filter out non english chars
+			#if /\w| |'/ == line
+			#else
+
+			#Step 5: set to lowercase
+			title.downcase!
+			puts title
 		end
 
 		puts "Finished. Bigram model built.\n"
